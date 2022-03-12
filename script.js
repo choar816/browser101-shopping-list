f86d78880240faffc7b12184d01563bf2081a6bc
Ahra Cho <choar816@postech.ac.kr>
2022-03-12 22:11:57 +0900

    Add: finished

---
+++ script.js
@@ -0,0 +1,39 @@
+const list = document.querySelector('.list');
+const input = document.querySelector('input');
+const addBtn = document.querySelector('.add');
+
+function addItem(inputText) {
+  const item = document.createElement('li');
+  const text = document.createElement('p');
+  text.textContent = inputText;
+  const delBtn = document.createElement('button');
+  delBtn.classList.add('del');
+  delBtn.textContent = '❌';
+  
+  item.appendChild(text);
+  item.appendChild(delBtn);
+  list.appendChild(item);
+  item.scrollIntoView({block: 'end', behavior: 'smooth'});
+}
+
+function addItemAndResetInput() {
+  if (input.value === '')
+    return;
+  addItem(input.value);
+  input.value = '';
+}
+
+list.addEventListener('click', (e) => {
+  if (e.target.className === 'del') {
+    const itemToDelete = e.target.parentElement;
+    itemToDelete.remove();
+  }
+});
+
+addBtn.addEventListener('click', addItemAndResetInput);
+
+input.addEventListener('keypress', (key) => {
+  if (key.key == 'Enter') {
+    addItemAndResetInput();
+  }
+});
